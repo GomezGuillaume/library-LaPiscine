eb20806ba52a2d48738939ab3b87ab95fddb3e4f
GOMEZ Guillaume <guillaume.gomez@lapiscine.pro>
2020-07-10 12:26:29 +0200

    library

---
+++ src/Entity/Author.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ */
+class Author
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthname;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $deathdate;
+
+    /**
+     * @ORM\Column(type="text", length=255)
+     */
+    private $biography;
+
+    /**
+     * @ORM\Column(type="boolean", length=255)
+     */
+    private $published;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getBirthname(): ?\DateTimeInterface
+    {
+        return $this->birthname;
+    }
+
+    public function setBirthname(\DateTimeInterface $birthname): self
+    {
+        $this->birthname = $birthname;
+
+        return $this;
+    }
+
+    public function getDeathdate(): ?\DateTimeInterface
+    {
+        return $this->deathdate;
+    }
+
+    public function setDeathdate(\DateTimeInterface $deathdate): self
+    {
+        $this->deathdate = $deathdate;
+
+        return $this;
+    }
+
+    public function getBiography(): ?string
+    {
+        return $this->biography;
+    }
+
+    public function setBiography(string $biography): self
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    public function getPublished(): ?string
+    {
+        return $this->published;
+    }
+
+    public function setPublished(string $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+}
